e39b6150821a0b49c4f5019fbc4421a9ed8b7350
Tamás Tibold <tiboldtomi94@gmail.com>
2019-02-22 22:18:34 +0100

    Updates

---
+++ precache-manifest.17722efed3a4401fe5a5de799fb2a309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "577c4fa8e97dfb0d78ef",
+    "url": "/home-championship/static/js/runtime~main.577c4fa8.js"
+  },
+  {
+    "revision": "661124b68db9b58f41d1",
+    "url": "/home-championship/static/js/main.661124b6.chunk.js"
+  },
+  {
+    "revision": "d04e85d438a19e2d5f41",
+    "url": "/home-championship/static/js/2.d04e85d4.chunk.js"
+  },
+  {
+    "revision": "661124b68db9b58f41d1",
+    "url": "/home-championship/static/css/main.aec6f3f5.chunk.css"
+  },
+  {
+    "revision": "61de8165fe3e2d3e0d2488335b51c7a7",
+    "url": "/home-championship/index.html"
+  }
+];
